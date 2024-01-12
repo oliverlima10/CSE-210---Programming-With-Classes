13391cd5bce2010802c783e63d65ad368fe919e0
oliverlima10 <oliverlima10@gmail.com>
2024-01-12 12:53:41 -0300

    Prep 3 Assigment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        Console.WriteLine("What is you guess? ");
+        string userGuess = Console.ReadLine();
+        
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is you guess? ")
+            guess = int.Parse(Console.ReadLine())
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Try Lower");
+
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Try Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it");
+
+            }
+
+        }
+
+       
+
+
+
+    
+
     }
 }
