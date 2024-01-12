83ab12d5f1a5d2f6f44e15e87aada56d585ad920
oliverlima10 <oliverlima10@gmail.com>
2024-01-12 14:05:29 -0300

    Correction Display Function

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,7 +5,7 @@ class Program
 
     static void Main(string[] args)
     {
-       DisplayWelcomeMessage();
+       DisplayWelcome();
 
         string userName = PromptUserName();
         int userNumber = PromptUserNumber();
