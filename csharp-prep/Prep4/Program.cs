59884a00fee3ce6a90fed509fcdbc088d94c916c
oliverlima10 <oliverlima10@gmail.com>
2024-01-12 13:27:45 -0300

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a List number. Type 0 to finish");
+        
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+             Console.Write("Enter with a number: ");
+             string userAnswer = Console.ReadLine();
+             userNumber = int.Parse(userAnswer);
+
+             if (userNumber !=0)
+             {
+                numbers.Add(userNumber);
+             }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The total sum is {sum}");
+
+        float avarageNumbers = (float(sum)) / numbers.Count; 
+        Console.WriteLine($"The avarage among the numbers is {avarageNumbers}")
+
+        int maxNumber = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {maxNumber}");
+        
+    
+
     }
 }
